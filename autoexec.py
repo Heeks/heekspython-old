aa46dde26b53be6948beaf2e5e5242b17f94624c
danheeks <danheeks@ae8bab52-3526-11de-9d05-31133e6853b1>
2011-04-03 07:27:19 +0000

    I commented out the contents of autoexec.py; it was just an experiment for using HeeksPython from HeeksCNC python scripts.

--- autoexec.py
+++ autoexec.py
@@ -1,11 +1,11 @@
-import platform
-import sys
-if platform.system() == "Windows":
-	sys.path.insert(0,'C:\\Users\\Dan\\HeeksCNC')
-else:
-	sys.path.insert(0,'/usr/local/share/heekspython/heeksscripts')
+#import platform
+#import sys
+#if platform.system() == "Windows":
+#	sys.path.insert(0,'C:\\Users\\Dan\\HeeksCNC')
+#else:
+#	sys.path.insert(0,'/usr/local/share/heekspython/heeksscripts')
 
-import HeeksCNC
-from HeeksCAD import HeeksCAD
-HeeksCNC.cad = HeeksCAD()
-HeeksCNC.start()
+#import HeeksCNC
+#from HeeksCAD import HeeksCAD
+#HeeksCNC.cad = HeeksCAD()
+#HeeksCNC.start()
